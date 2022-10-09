fab46bc46d104fef49ab5ce2c3fb20bb9037b199
Stud.net <st.codding@gmail.com>
2022-10-09 13:42:02 +0300

    Sticky navbar: project is completed

--- Projects/Sticky navbar/js/script.js
+++ Projects/Sticky navbar/js/script.js
@@ -1,6 +1,6 @@
 'use strict'; 
 
-const headerBox = document.querySelector('.header__container');
+const headerBox = document.querySelector('.header__box');
 
 window.addEventListener('scroll', function () {
 	this.scrollY > 20 ? headerBox.classList.add('sticky') : headerBox.classList.remove('sticky');
